d27d37daae21b8085dacc6551d7526dd6d9cd609
blingblin-g <luvbliny@gmail.com>
2021-05-18 00:49:45 +0900

    Feat: Profile box
    가장 왼쪽에 있는 프로필 박스 컴포넌트
    (안에 내용물은 추후에 채워넣어야함)

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -2,32 +2,64 @@
 
 import styled from 'styled-components';
 //import Button from '@material-ui/core/Button';
-//import Box from '@material-ui/core/Box';
+import Box from '@material-ui/core/Box';
 import Logo from '../components/common/Logo';
 import UserName from '../components/common/UserName';
 //import Calendar from './Calendar';
 
-//function UserInfo() {}
+function Hello({ username }) {
+  return <StyledHello>{username}님 안녕하세요!</StyledHello>;
+}
+
+const StyledHello = styled('div')`
+  font-family: Rubik;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 30px;
+  line-height: 40px;
+`;
+
+function Profile({ username }) {
+  return (
+    <ProfileBox>
+      {/*// 이미지
+			// 이름
+			// 별명?
+			// 이상한 숫자??
+			// 나의 코디*/}
+    </ProfileBox>
+  );
+}
+
+const ProfileBox = styled(Box)`
+  width: 364px;
+  height: 380px;
+  left: 131px;
+  top: 216px;
+
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 30px;
+`;
 
 function Main() {
   // api server에서 username을 받아와야함
   //  const [username, setUsername] = useState();
-  const username = '임채욱';
+  const username = '김윤주';
   return (
     <MainLayout>
       <Header>
         <Logo />
         <UserName username={username} />
       </Header>
-      {/*<Body>
+      <Body>
         <UserInfo>
-          <Hello />
+          <Hello username={username} />
           <Profile />
-          <Wether />
+          {/*<Wether />*/}
         </UserInfo>
-        <TodayOOTD></TodayOOTD>
-        <Calendar></Calendar>
-      </Body>*/}
+        {/*<TodayOOTD></TodayOOTD>
+        <Calendar></Calendar>*/}
+      </Body>
     </MainLayout>
   );
 }
@@ -43,18 +75,18 @@ const Header = styled('div')`
   justify-content: space-between;
 `;
 
-//const Body = styled('div')`
-//  display: flex;
-//  flex-direction: row;
-//  justify-content: center;
-//  align-items: center;
-//`;
+const Body = styled('div')`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
 
-//const UserInfo = styled('div')`
-//  display: flex;
-//  flex-direction: column;
-//  justify-content: center;
-//  align-items: center;
-//`;
+const UserInfo = styled('div')`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default Main;
