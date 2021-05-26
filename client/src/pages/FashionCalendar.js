0eeca1312a3d16900727809b53693dfbaee92a3e
blingblin-g <luvbliny@gmail.com>
2021-05-27 02:10:42 +0900

    Fix calendar
    month 버튼이 안눌려지는 버그 해결
    next, prev label 컬러 바꿈
    
    
    Former-commit-id: 3f250006ab2a36060b4b14404a35b288b6f8aba2

--- client/src/pages/FashionCalendar.js
+++ client/src/pages/FashionCalendar.js
@@ -23,12 +23,11 @@ function FashionCalendar() {
   const [value, setValue] = useState(new Date());
   const [dates, setDates] = useState([]);
   const history = useHistory();
-  const username = '김윤주';
   const userId = 1;
 
   //데이터가 없는 날짜는 비활성화 시킨다.
-  const noData = ({ activeStartDate, date, view }) => {
-    return !dates.some((d) => d === getFormatDate(date));
+  const isData = ({ date, view }) => {
+    return view === 'month' && !dates.some((d) => d === getFormatDate(date));
   };
 
   // 날짜를 클릭했을 때, 페이지 전환 후 해당 날짜에 해당하는 데이터를 서버에 요청. 쿼리스트링 방식.
@@ -37,32 +36,47 @@ function FashionCalendar() {
     history.push(`/dashboard?user_id=${userId}&date=${getFormatDate(date)}`);
   };
 
-  const getDate = async () => {
-    try {
-      const { data } = await axios.get(`${URL}/calendar/${userId}`);
-      setDates(data.ootd_enrolled_dates);
-    } catch (e) {
-      console.error(e);
-      setDates(['2021-05-01', '2021-05-25', '2021-05-17', '2021-04-11']);
-    }
-  };
-
   useEffect(() => {
+    const getDate = async () => {
+      try {
+        const { data } = await axios.get(`${URL}/calendar`, {
+          params: {
+            user_id: userId,
+            month: value.getMonth()
+            // 이전 달과 다음 달의 데이터도 필요함.
+          }
+        });
+        setDates(data.ootd_enrolled_dates);
+      } catch (e) {
+        console.error(e);
+        setDates([
+          '2021-04-03',
+          '2021-05-01',
+          '2021-05-25',
+          '2021-05-17',
+          '2021-04-11'
+        ]);
+      }
+    };
     getDate();
-  }, []);
+  }, [value]);
 
   return (
     <FlexDiv>
       <Calendar
-        onChange={setValue}
+        //onChange={setValue}
         onClickDay={onClickDay}
-        nextLabel={'▶'}
-        next2Label={'▷'}
-        prevLabel={'◀'}
-        prev2Label={'◁'}
+        nextLabel={<NextLabel />}
+        next2Label={<Next2Label />}
+        prevLabel={<PrevLabel />}
+        prev2Label={<Prev2Label />}
         value={value}
-        tileDisabled={noData}
+        tileDisabled={isData}
         tileClassName={tileClassName}
+        onActiveStartDateChange={({ activeStartDate }) => {
+          setValue(activeStartDate);
+          console.log(activeStartDate);
+        }}
       />
     </FlexDiv>
   );
@@ -73,5 +87,24 @@ const FlexDiv = styled('div')`
   width: 100%;
   flex-direction: column;
 `;
+
+const StyledLabel = {
+  color: '#8f00ff'
+};
+
+function NextLabel() {
+  return <div style={StyledLabel}>▶</div>;
+}
+
+function Next2Label() {
+  return <div style={StyledLabel}>▷</div>;
+}
+
+function PrevLabel() {
+  return <div style={StyledLabel}>◀</div>;
+}
+function Prev2Label() {
+  return <div style={StyledLabel}>◁</div>;
+}
 
 export default FashionCalendar;
