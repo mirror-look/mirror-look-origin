957717b5070a0c248a7ae1bd2fa758694e390f8b
SeongminHa <seongminha0910@gmail.com>
2021-05-27 07:44:59 +0000

    refactor: Header 내 userName 코드 수정
    Former-commit-id: 4d8ebe084622e01a042d9b34f0c0219be08460a5

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,8 +1,28 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 import Logo from './Logo';
 import UserName from './UserName';
 
-function Header({ userName }) {
+function Header() {
+  const [userName, setUserName] = useState();
+  useEffect(() => {
+    const token = `Bearer ${window.sessionStorage.getItem('token')}`;
+    axios
+      .get('http://localhost:64964/userinfo', {
+        headers: {
+          Authorization: token
+        }
+      })
+      .then(function (response) {
+        console.log('헤더에 사용자 이름 받아왔다!');
+        setUserName(response.data.user_info.user_name);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <StyledHeader>
       <Logo />
