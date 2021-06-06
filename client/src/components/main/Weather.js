0de23a0e6569257073e19dcb1e235507bcb1a00f
SeongminHa <seongminha0910@gmail.com>
2021-06-06 08:40:46 +0000

    refactor: Weather 컴포넌트 분리
    Former-commit-id: e965c319a17e6cfe8d2c922c0bea1e65c74bb0de

---
+++ client/src/components/main/Weather.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
+
+function Weather({ lat, lng }) {
+  const customStyles = {
+    fontFamily: 'Helvetica, sans-serif',
+    gradientStart: '#7F7FD5',
+    gradientMid: '#86A8E7',
+    gradientEnd: '#91EAE4',
+    locationFontColor: '#FFF',
+    todayTempFontColor: '#FFF',
+    todayDateFontColor: '#B5DEF4',
+    todayRangeFontColor: '#B5DEF4',
+    todayDescFontColor: '#B5DEF4',
+    todayInfoFontColor: '#B5DEF4',
+    todayIconColor: '#FFF',
+    forecastBackgroundColor: '#FFF',
+    forecastSeparatorColor: '#DDD',
+    forecastDateColor: '#777',
+    forecastDescColor: '#777',
+    forecastRangeColor: '#777',
+    forecastIconColor: '#4BC4F7'
+  };
+
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: '83728bc30509bca1ea0aaba3ca6c54c0',
+    lat: String(lat),
+    lon: String(lng),
+    lang: 'en',
+    unit: 'metric'
+  });
+
+  return (
+    <div>
+      <ReactWeather
+        theme={customStyles}
+        isLoading={isLoading}
+        errorMessage={errorMessage}
+        data={data}
+        lang="en"
+        unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
+        showForecast={false}
+      />
+    </div>
+  );
+}
+
+export default Weather;
