1fefc542d989ea2f3a274f49041e7e8e889eaa32
SeongminHa <seongminha0910@gmail.com>
2021-06-09 16:05:47 +0000

    fix: Recommend API Params 수정
    Former-commit-id: 9a2dee54fbd972f50816f89b51a1d4b4e5c1d4cb

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -86,6 +86,7 @@ function Detail() {
   const history = useHistory();
   const userInfo = useSelector((store) => store.userInfoReducer);
   const imageFullPath = useSelector((store) => store.imagePathReducer);
+  const userSelects = useSelector((store) => store.userSelectsReducer);
   const imagePath = useSelector((store) => store.imageBase64Reducer);
   const userName = '김윤주';
   const location = useLocation();
@@ -95,12 +96,16 @@ function Detail() {
 
   function handleClick() {
     const date = new Date();
+    let subCategory = [];
+    for (let key in userSelects) {
+      subCategory.push(userSelects[key]);
+    }
     console.log('캘린더에 저장하기 클릭했다!');
     axios
       .post('http://localhost:5000/calendar', {
         ootd_img_path: imageFullPath,
         user_id: userInfo.user_id,
-        clothes_subcategory: location.state.clothes_for_weather,
+        clothes_subcategory: subCategory,
         date: date
       })
       .then(function (response) {
